48cd50a19018b491cbb2d17a804ee883424a1d6f
Steven Luo <steven@steven676.net>
2021-06-18 23:35:27 -0700

    Move issuer public key into a separate module
    This is in preparation for the ability to handle multiple issuers.

---
+++ src/issuer_keys.js
@@ -0,0 +1,14 @@
+const issuerKeys = [
+  {
+    id: "ca.qc-001",
+    iss: "", // XXX
+    kid: "some-kid",
+    alg: "ES256", kty: "EC", crv: "P-256", use: "sig",
+    x: "XSxuwW_VI_s6lAw6LAlL8N7REGzQd_zXeIVDHP_j_Do",
+    y: "88-aI4WAEl4YmUpew40a9vq_w5OcFvsuaKMxJRLRLL0",
+  },
+];
+
+module.exports = {
+  issuerKeys,
+};
