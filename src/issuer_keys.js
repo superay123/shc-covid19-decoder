4e55b21e43a0eacec167c55e9a01ca4b5ffedb78
Steven Luo <steven@steven676.net>
2021-07-19 03:35:21 -0700

    Fill in the correct kid for Quebec's signing key
    Transcribed from the JWS header output in demo.png in this repository.

--- src/issuer_keys.js
+++ src/issuer_keys.js
@@ -2,7 +2,7 @@ const issuerKeys = [
   {
     id: "ca.qc-001",
     iss: "", // XXX
-    kid: "some-kid",
+    kid: "o-kjQwit6lT5I1aSzWd26SdN3FANWtwUyxKuiyEXP68",
     alg: "ES256", kty: "EC", crv: "P-256", use: "sig",
     x: "XSxuwW_VI_s6lAw6LAlL8N7REGzQd_zXeIVDHP_j_Do",
     y: "88-aI4WAEl4YmUpew40a9vq_w5OcFvsuaKMxJRLRLL0",
