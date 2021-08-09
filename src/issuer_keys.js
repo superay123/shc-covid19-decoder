4d47866c3941f57f31e3d9b36115d74e641f8847
Raymond Ménard <88417731+superay123@users.noreply.github.com>
2021-08-08 23:32:56 -0400

    Enter Quebec QR code iss string.js. The kid value is now stable since mid July.

--- src/issuer_keys.js
+++ src/issuer_keys.js
@@ -1,8 +1,8 @@
 const issuerKeys = [
   {
     id: "ca.qc-001",
-    iss: "", // XXX
-    kid: "o-kjQwit6lT5I1aSzWd26SdN3FANWtwUyxKuiyEXP68",
+    iss: "https://covid19.quebec.ca/PreuveVaccinaleApi/issuer", 
+    kid: "qFdl0tDZK9JAWP6g9_cAv57c3KWxMKwvxCrRVSzcxvM",
     alg: "ES256", kty: "EC", crv: "P-256", use: "sig",
     x: "XSxuwW_VI_s6lAw6LAlL8N7REGzQd_zXeIVDHP_j_Do",
     y: "88-aI4WAEl4YmUpew40a9vq_w5OcFvsuaKMxJRLRLL0",
